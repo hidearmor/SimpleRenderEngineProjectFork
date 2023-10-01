1ed8d17fcce5d579fde080eaf7cd9be708d8d7ed
jrya <jrya@itu.dk>
2023-10-01 17:56:21 +0200

    can move around but wrong way

--- Exercise4/ExampleGame/Game/ComponentController.cpp
+++ Exercise4/ExampleGame/Game/ComponentController.cpp
@@ -25,10 +25,10 @@ namespace ExampleGame {
 		float smoothRotSpeed = 0.016666667f;
 		//parent->rotation += smoothRotSpeed * RotSpeed * deltaTime;
 		//if(ShouldRotate)
-			parent->rotation += RotSpeed * deltaTime;
+		parent->rotation += RotSpeed * deltaTime;
 		//parent->rotation += std::atan2(MovDirection.y, MovDirection.x);
 		// Convert rotation to direction vector
-		//MovDirection = glm::vec2(cos(smoothRotSpeed * parent->rotation), sin(smoothRotSpeed * parent->rotation));
+		MovDirection = glm::vec2(cos(smoothRotSpeed * parent->rotation), sin(smoothRotSpeed * parent->rotation));
 		parent->position += MovDirection * MovAmount * MovSpeed * deltaTime;
 
 		// reset position
@@ -83,20 +83,24 @@ namespace ExampleGame {
 		switch (event.key.keysym.sym) {
 
 			case SDLK_a: {
-				if(event.type == SDL_KEYDOWN) 		SetRotationSpeed(100);
+				if(event.type == SDL_KEYDOWN) 		SetRotationSpeed(180);
 				else if (event.type == SDL_KEYUP) 	SetRotationSpeed(0);
 			}
 				break;
 			case SDLK_d: {
-				if(event.type == SDL_KEYDOWN) 		SetRotationSpeed(-100);
+				if(event.type == SDL_KEYDOWN) 		SetRotationSpeed(-180);
 				else if (event.type == SDL_KEYUP) 	SetRotationSpeed(0);
 			}
 				break;
 			case SDLK_w : {
-				if(event.type == SDL_KEYDOWN) 		SetMovementSpeed(150);
+				if(event.type == SDL_KEYDOWN) 		SetMovementSpeed(200);
 				else if (event.type == SDL_KEYUP) 	SetMovementSpeed(0);
 			}
 				break;
+			case SDLK_s : {
+				if(event.type == SDL_KEYDOWN) 		SetMovementSpeed(-150);
+				else if (event.type == SDL_KEYUP) 	SetMovementSpeed(0);
+			}
 			default: break;
 		}
 
